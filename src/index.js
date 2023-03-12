96feb3401b2e4ba73ff393c71f6b7fda900eff95
Janan Akkad <jananakkad@hotmail.co.uk>
2023-03-12 17:54:40 +0000

    commit tree 235be2cd0e5e73007200ba7f1c9f030f25adb287

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const createTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  const tasks = document.getElementById("tasks");
+
+  createTaskForm.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+    const list = tasks.appendChild(document.createElement('li'));
+    list.innerHTML = newTaskDescription.value;
+    
+  });
+
 });
