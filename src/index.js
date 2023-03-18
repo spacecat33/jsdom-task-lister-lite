b91680ae33d7cff9164596bc11354f210c9998cf
Janan Akkad <jananakkad@hotmail.co.uk>
2023-03-18 17:04:22 +0000

    attempting priority setting

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ document.addEventListener("DOMContentLoaded", () => {
   const createTaskForm = document.getElementById("create-task-form");
   const newTaskDescription = document.getElementById("new-task-description");
   const tasks = document.getElementById("tasks");
+  const priority = document.getElementById("priority")
 
   createTaskForm.addEventListener("submit", function(event) {
     event.preventDefault();
     const list = tasks.appendChild(document.createElement('li'));
-    list.innerHTML = newTaskDescription.value;
+    // list.innerHTML = newTaskDescription.value;
+    // if priority.value != 0,
+    // list.style.color = 'red', 
+    // else 'blue';
 
     const createButtonElement = document.createElement('button');
     createButtonElement.innerHTML = 'x';
